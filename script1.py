81a2cd2b2a21a1f93bfd9e4f67f5094ab95605ed
Mohammad Mortaji <s.mohammadmortaji@gmail.com>
2019-08-04 05:03:47 +0400

    Just completed

--- script1.py
+++ script1.py
@@ -10,15 +10,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from os import listdir
-from keras.preprocessing import sequence
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
 
-from keras.optimizers import Adam
-from keras.models import load_model
-from keras.callbacks import ModelCheckpoint
+from tensorflow.keras.preprocessing import sequence
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 #making data a one sequence.
 path = 'MovementAAL/dataset/MovementAAL_RSS_'
@@ -52,3 +52,57 @@ final_seq = np.stack(new_seq)
 #truncate the sequence to length 60
 seq_len = 60
 final_seq=sequence.pad_sequences(final_seq, maxlen=seq_len, padding='post', dtype='float', truncating='post')
+
+
+train = [final_seq[i] for i in range(len(groups)) if (groups[i]==2)]
+validation = [final_seq[i] for i in range(len(groups)) if groups[i]==1]
+test = [final_seq[i] for i in range(len(groups)) if groups[i]==3]
+
+train_target = [targets[i] for i in range(len(groups)) if (groups[i]==2)]
+validation_target = [targets[i] for i in range(len(groups)) if groups[i]==1]
+test_target = [targets[i] for i in range(len(groups)) if groups[i]==3]
+
+
+train = np.array(train)
+validation = np.array(validation)
+test = np.array(test)
+
+train_target = np.array(train_target)
+train_target = (train_target+1)/2
+
+validation_target = np.array(validation_target)
+validation_target = (validation_target+1)/2
+
+test_target = np.array(test_target)
+test_target = (test_target+1)/2
+
+#building the model
+model = Sequential()
+model.add(LSTM(256,
+               input_shape=(seq_len,
+                            4)))
+model.add(Dense(1,
+                activation='sigmoid'))
+
+#Lets fit it!
+adam = Adam(lr=0.001)
+chk = ModelCheckpoint('best_model.pkl',
+                      monitor='val_acc',
+                      save_best_only=True,
+                      mode='max',
+                      verbose=1)
+model.compile(loss='binary_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+model.fit(train,
+          train_target,
+          epochs=200,
+          batch_size=128,
+          callbacks=[chk],
+          validation_data=(validation,
+          validation_target))
+
+#evaluation
+from sklearn.metrics import accuracy_score
+test_preds = model.predict_classes(test)
+sklearn.metrics.accuracy_score(test_target, test_preds)
